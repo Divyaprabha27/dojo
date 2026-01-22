199265ca791cefd2c3bfb08168fcb7b69686d493
Divyaprabha27 <divyaprabha.it2014@gmail.com>
2026-01-22 19:28:50 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggleBtn.innerHTML = "☀️";
+}
+
+// Toggle theme
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.innerHTML = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.innerHTML = "🌙";
+  }
+});
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+// Show button on scroll
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+// Scroll to top on click
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
